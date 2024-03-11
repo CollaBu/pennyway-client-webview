f8edd0afd865997f52cabd8f4526a9fada040889
이의찬 <cksxkr5193@naver.com>
2024-03-11 20:20:42 +0900

    초기 프로젝트 설정 (#2)
    * feat: 초기 Project 설정 
    
    yarn berry와 vite를 사용해서 Project를 생성했습니다.
    React Router와 React Query 등 필요 라이브러리를 설치했습니다.
    합의사항에 따라 FSD를 커스텀하여 폴더구조를 설정하였습니다.
    Readme에 기본적인 프로젝트 설명을 작성했습니다.
    
    Closes #PW-107

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.tsx";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
