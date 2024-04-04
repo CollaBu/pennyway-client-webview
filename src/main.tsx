8450829b57e0df350a31d93396f97ba461dc25b1
Uichan Lee <cksxkr5193@naver.com>
2024-04-04 21:13:06 +0900

    feat: main.tsx파일에 QueryProvider와 QueryDevtools 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./App.tsx";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.tsx';
+import './index.css';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <App />,
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+    <ReactQueryDevtools />
+  </React.StrictMode>,
 );
