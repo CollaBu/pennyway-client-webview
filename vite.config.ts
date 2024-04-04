165e4a252fac8662e5aec0d615580817e9d6bfc4
Uichan Lee <cksxkr5193@naver.com>
2024-04-04 21:12:56 +0900

    feat: path alias 설정을 위해 vite.config, tsconfig 파일에 경로 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import path from 'path';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+      { find: '@public', replacement: path.resolve(__dirname, 'public') },
+    ],
+  },
+});
