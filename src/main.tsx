928e3cb948a70ef6cbb5862c9f1c57767fdce28d
Uichan Lee <cksxkr5193@naver.com>
2024-04-05 19:37:27 +0900

    feat: queryClient와 RouterProvider를 main.tsx에서 import해 사용하도록 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import App from './App.tsx';
+import { RouterProvider } from 'react-router-dom';
+
 import './index.css';
-import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
-]);
-
-const queryClient = new QueryClient();
+import { queryClient } from './app/providers/query-client.ts';
+import { router } from './app/routers/index.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
