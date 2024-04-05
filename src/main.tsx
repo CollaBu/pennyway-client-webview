8921705c46f530001ffb745e19f5106f841d86bc
Uichan Lee <cksxkr5193@naver.com>
2024-04-05 19:55:16 +0900

    feat: 모듈 내 응집도를 고려해 QueryClientProvider와 RouterProvider를 App.tsx에서 관리하도록 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-
-import './index.css';
-import { QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { queryClient } from './app/providers/query-client.ts';
-import { router } from './app/routers/index.ts';
+import App from './app/App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
-    <ReactQueryDevtools />
+    <App />
   </React.StrictMode>,
 );
