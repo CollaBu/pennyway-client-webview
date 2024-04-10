f96266a9054c7c0f4d39bb4cebbd852c9a8732d3
BangDori <44726494+BangDori@users.noreply.github.com>
2024-04-10 16:04:47 +0900

    🛠️ CI/CD 파이프라인 구축 (#5)
    node_modules를 통한 의존성 관리 -> 추후 변경 예정 #6
    vitest와 eslint를 이용하여 CI Pipeline 구축
    docker와 EC2를 이용하여 CD Pipeline 구축
    
    Closes #PW-248

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./src/setupTest.tsx'],
+    setupFiles: ['./src/setupTest.ts'],
   },
   resolve: {
     alias: [
