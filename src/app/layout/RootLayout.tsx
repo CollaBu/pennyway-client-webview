d7c6721fe59f786ab5e0f4c0920d00c13e542d07
Uichan Lee <cksxkr5193@naver.com>
2024-04-11 22:19:55 +0900

    feat: iOS에서 Navbar를 구현할 공간을 고려하여 RootLayout에 Navbar 추가

--- src/app/layout/RootLayout.tsx
+++ src/app/layout/RootLayout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
+import Navbar from '@/shared/ui/Navbar';
 
 export default function RootLayout() {
   return (
     <>
       <Outlet />
+      <Navbar />
     </>
   );
 }
