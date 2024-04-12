0117087317eee42846a765a91157b3d3a59d5791
이의찬 <cksxkr5193@naver.com>
2024-04-12 16:05:53 +0900

    🎨 스타일 설정 (#7)
    - font 설치 및 mixin을 사용해 font class 설정
    용량을 줄이고 빠르게 폰트를 가져올 수 있도록 subset font를 사용했습니다.
    용량이 50%가량 더 적은 woff2를 기본 폰트로 하고, woff를 fallback font로 지정했습니다
    
    - color 변수 설정
    
    - iOS에서 Navbar를 구현할 공간을 고려하여 RootLayout에 FNB 컴포넌트 추가
    shared/ui에 많은 컴포넌트가 들어갈 수 있기에 추가적으로 depth를 만들었습니다.
    
    - 의도하지 않은 확대를 막기위해 user-scalable=no 설정
    
    - 전처리기인 sass의 특성을 고려해 devDependencies로 재설치
    
    Closes #PW-250

--- src/app/layout/RootLayout.tsx
+++ src/app/layout/RootLayout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
+import FNB from '@/shared/ui/FNB/FNB';
 
 export default function RootLayout() {
   return (
     <>
       <Outlet />
+      <FNB />
     </>
   );
 }
