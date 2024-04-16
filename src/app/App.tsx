90c41cef5055f0543fdf7d2ab33933fb57f109e6
BangDori <qudwns8865@naver.com>
2024-04-16 18:43:02 +0900

    feat: mock worker 지연 처리

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,12 +4,6 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { queryClient } from './providers/query-client';
 import { router } from './routers/index';
 
-if (import.meta.env.DEV) {
-  // development 모드일 경우 Mocking 서버가 실행됩니다.
-  const { worker } = await import('@/shared/mocks/browser');
-  worker.start();
-}
-
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app/App';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+async function enableMocking() {
+  if (import.meta.env.PROD) {
+    return;
+  }
+
+  const { worker } = await import('@/shared/mocks/browser');
+
+  return worker.start();
+}
+
+enableMocking().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+});
