be96b1028bc2ef92d12d3b4ce7d06603ba1623e3
BangDori <qudwns8865@naver.com>
2024-04-19 16:57:02 +0900

    test: isLike -> isLiked 변수명 수정

--- src/app/mocks/test/like/like.test.ts
+++ src/app/mocks/test/like/like.test.ts
@@ -3,9 +3,9 @@ import { describe, expect, it } from 'vitest';
 
 async function getFeedById(feedId: number) {
   const likes = await axios.get(`/feeds/${feedId}/likes`);
-  const { totalCount, isLike } = likes.data.data.like;
+  const { totalCount, isLiked } = likes.data.data.like;
 
-  return { totalCount, isLike };
+  return { totalCount, isLiked };
 }
 
 describe('좋아요가 되어있지 않은 상태에서', () => {
@@ -18,38 +18,38 @@ describe('좋아요가 되어있지 않은 상태에서', () => {
     await axios.put(`/feeds/${feedId}/likes`);
 
     // 업데이트 된 좋아요의 상태
-    const { totalCount, isLike } = await getFeedById(feedId);
+    const { totalCount, isLiked } = await getFeedById(feedId);
 
     expect(totalCount).toBe(prevTotalCount + 1);
-    expect(isLike).toBeTruthy();
+    expect(isLiked).toBeTruthy();
   });
 
   it('좋아요 취소를 클릭하면, 좋아요의 수가 변경되지 않는다.', async () => {
     const feedId = 3;
-    const { totalCount: prevTotalCount, isLike: prevIsLike } =
+    const { totalCount: prevTotalCount, isLiked: prevIsLiked } =
       await getFeedById(feedId);
 
     await axios.delete(`/feeds/${feedId}/likes`);
 
-    const { totalCount, isLike } = await getFeedById(feedId);
+    const { totalCount, isLiked } = await getFeedById(feedId);
 
     expect(totalCount).toBe(prevTotalCount);
-    expect(isLike).toBe(prevIsLike);
+    expect(isLiked).toBe(prevIsLiked);
   });
 });
 
 describe('좋아요가 되어있는 상태에서', () => {
   it('좋아요를 클릭하면, 좋아요의 수가 변동되지 않는다.', async () => {
     const feedId = 2;
-    const { totalCount: prevTotalCount, isLike: prevIsLike } =
+    const { totalCount: prevTotalCount, isLiked: prevIsLiked } =
       await getFeedById(feedId);
 
     await axios.put(`/feeds/${feedId}/likes`);
 
-    const { totalCount, isLike } = await getFeedById(feedId);
+    const { totalCount, isLiked } = await getFeedById(feedId);
 
     expect(totalCount).toBe(prevTotalCount);
-    expect(isLike).toBe(prevIsLike);
+    expect(isLiked).toBe(prevIsLiked);
   });
 
   it('좋아요 취소를 클릭하면, 좋아요의 수가 1 감소한다.', async () => {
@@ -58,9 +58,9 @@ describe('좋아요가 되어있는 상태에서', () => {
 
     await axios.delete(`/feeds/${feedId}/likes`);
 
-    const { totalCount, isLike } = await getFeedById(feedId);
+    const { totalCount, isLiked } = await getFeedById(feedId);
 
     expect(totalCount).toBe(prevTotalCount - 1);
-    expect(isLike).toBeFalsy();
+    expect(isLiked).toBeFalsy();
   });
 });
