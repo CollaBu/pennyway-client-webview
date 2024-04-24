95cdbf1104315b622d2c7dd260d5aaeb2e643a18
Uichan Lee <cksxkr5193@naver.com>
2024-04-24 23:41:30 +0900

    feat: vite-plugin-svgr를 tsconfig.json과 vite.config.ts에 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import path from 'path';
 
 import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 import { defineConfig } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   test: {
     globals: true,
     environment: 'jsdom',
