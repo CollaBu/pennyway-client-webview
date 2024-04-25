2bfdf2167ca9cf9323afda43481d0d95e951b3ad
Uichan Lee <cksxkr5193@naver.com>
2024-04-25 13:10:40 +0900

    feat: PageHeader 컴포넌트 구현
    {page} 중앙 정렬을 위해 우측에 dummy div 태그 사용

---
+++ src/shared/ui/header/PageHeader.tsx
@@ -0,0 +1,20 @@
+import BackIcon from '@public/assets/icon/back-icon.svg?react';
+import { Link } from 'react-router-dom';
+import './PageHeader.scss';
+
+interface PageHeaderProps {
+  prevPageLink: string;
+  page: string;
+}
+
+export default function PageHeader({ prevPageLink, page }: PageHeaderProps) {
+  return (
+    <div className='pageHeader'>
+      <Link to={prevPageLink}>
+        <BackIcon />
+      </Link>
+      <p className='page'>{page}</p>
+      <div className='dummy'></div>
+    </div>
+  );
+}
