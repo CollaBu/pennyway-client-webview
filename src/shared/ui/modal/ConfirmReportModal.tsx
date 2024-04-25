93a50e814d81b9a8190cb30cbba9ee3412f610ce
Uichan Lee <cksxkr5193@naver.com>
2024-04-25 14:43:52 +0900

    feat: 변경 가능성 고려 ConfirmReportModal 구현

---
+++ src/shared/ui/modal/ConfirmReportModal.tsx
@@ -0,0 +1,33 @@
+import ConfirmButton from '../button/ConfirmButton';
+import ConfirmCancleButton from '../button/ConfirmCancleButton';
+
+import { BaseModalProps } from './types';
+
+interface ConfirmReportModalProps extends BaseModalProps {
+  children: React.ReactNode;
+}
+
+export default function ConfirmReportModal({
+  title,
+  children,
+  onExecute,
+  onExecuteMsg,
+  onExecuteisActive,
+  onClose,
+  onCloseMsg,
+}: ConfirmReportModalProps) {
+  return (
+    <div className='confirmReportModal'>
+      <h3 className='title h3semi'>{title}</h3>
+      <section>{children}</section>
+      <div className='buttons'>
+        <ConfirmCancleButton onClick={onClose}>
+          {onCloseMsg}
+        </ConfirmCancleButton>
+        <ConfirmButton onClick={onExecute} isActive={onExecuteisActive}>
+          {onExecuteMsg}
+        </ConfirmButton>
+      </div>
+    </div>
+  );
+}
