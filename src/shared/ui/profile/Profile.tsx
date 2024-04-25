765ecb7f6f56d970b4fd963155e08db856ac6939
Uichan Lee <cksxkr5193@naver.com>
2024-04-25 22:22:36 +0900

    feat: Profile styleclass props와 일치하게 이름 수정

--- src/shared/ui/profile/Profile.tsx
+++ src/shared/ui/profile/Profile.tsx
@@ -11,8 +11,8 @@ export default function Profile({ profileImage, name, content }: ProfileProps) {
     <div className='profile'>
       <img src={profileImage} className='profileImage' />
       <div className='nameSection'>
-        <h5 className='userName b1semi'>{name}</h5>
-        <p className='description b3md'>{content}</p>
+        <h5 className='name b1semi'>{name}</h5>
+        <p className='content b3md'>{content}</p>
       </div>
     </div>
   );
