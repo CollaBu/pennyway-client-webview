5e58b462b9126af4cb7858a794c90fabef5d0369
Uichan Lee <cksxkr5193@naver.com>
2024-04-29 19:17:19 +0900

    feat: Modal에 상위요소가 영향을 미치지 않도록 ModalOverlay 컴포넌트 구현
    Modal이 상위요소의 위치에 따라 영향을 받지 않도록 createPortal을 사용한 ModalOverlay 컴포넌트를 구현했습니다.

---
+++ src/shared/ui/modal/ModalOverlay.tsx
@@ -0,0 +1,32 @@
+import { createPortal } from 'react-dom';
+
+import { Backdrop } from '../backdrop';
+import './ModalOverlay.scss';
+
+const portalElement: HTMLElement = document.getElementById(
+  'overlays',
+) as HTMLElement;
+
+type ModalOverlayClass = 'bottom-sheet' | 'modal';
+
+interface ModalOverlayProps {
+  styleClass: ModalOverlayClass;
+  children: JSX.Element;
+  onClose: () => void;
+}
+
+export const ModalOverlay = ({
+  styleClass,
+  children,
+  onClose,
+}: ModalOverlayProps) => {
+  return (
+    <>
+      {createPortal(<Backdrop onClick={onClose} />, portalElement)}
+      {createPortal(
+        <figure className={styleClass}>{children}</figure>,
+        portalElement,
+      )}
+    </>
+  );
+};
