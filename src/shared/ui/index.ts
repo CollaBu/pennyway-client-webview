a5c5228f379d6509265a340fdb202db5ddae3f6b
Uichan Lee <cksxkr5193@naver.com>
2024-04-30 01:06:55 +0900

    fix: shared/ui/index 수정

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -2,4 +2,4 @@ export { ActiveButton, BasicButton } from './button';
 export { FNB } from './fnb';
 export { PageHeader } from './header';
 export { ConfirmModal, ConfirmReportModal, BottomSheetModal } from './modal';
-export { Profile, ProfileImage } from './profile';
+export { Profile } from './profile';
