6ecb75a7fdd4aa6445425a7f3f6d07e5bbcaaebc
Uichan Lee <cksxkr5193@naver.com>
2024-04-30 00:37:08 +0900

    feat: 잘못 export한 button type들 삭제

--- src/shared/ui/button/index.ts
+++ src/shared/ui/button/index.ts
@@ -1,4 +1,2 @@
 export { ActiveButton } from './ActiveButton';
-export type { ActiveStyle } from './ActiveButton';
 export { BasicButton } from './BasicButton';
-export type { BasicStyle } from './BasicButton';
