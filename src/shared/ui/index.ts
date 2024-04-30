650e5ed666b463e137750b20765b977fa37daa65
BangDori <qudwns8865@naver.com>
2024-04-30 18:58:13 +0900

    feat: public api FNB 제외

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,5 +1,4 @@
 export { ActiveButton, BasicButton } from './button';
-export { FNB } from './fnb';
 export { PageHeader } from './header';
 export { ConfirmModal, ConfirmReportModal, BottomSheetModal } from './modal';
 export { Profile } from './profile';
