499bdaf1cf59f1f1256161802fa69b9fc444d246
BangDori <qudwns8865@naver.com>
2024-04-30 18:59:19 +0900

    feat: 피드 메인 페이지 UI

---
+++ src/features/feed/ui/Feed.tsx
@@ -0,0 +1,59 @@
+import { Profile } from '@/shared/ui';
+import BookmarkIcon from '@/shared/ui/icon/assets/bookmark_icon.svg?react';
+import ChatIcon from '@/shared/ui/icon/assets/chat_icon.svg?react';
+import KebabMenuIcon from '@/shared/ui/icon/assets/kebab_menu_icon.svg?react';
+import LikeIcon from '@/shared/ui/icon/assets/like_icon.svg?react';
+import SharedIcon from '@/shared/ui/icon/assets/share_icon.svg?react';
+
+import './Feed.scss';
+import { FeedProps } from '../consts/type';
+
+const Feed: React.FC<FeedProps> = ({ feed }) => {
+  const { user, content, likeCount, commentCount, updatedAt } = feed;
+
+  return (
+    <div className='feed-wrapper'>
+      <article className='feed-article'>
+        <header className='feed-header'>
+          <Profile
+            profileImage={user.profileImage}
+            name={user.name}
+            content={updatedAt}
+          />
+
+          <button className='kebab-icon-btn'>
+            <KebabMenuIcon />
+          </button>
+        </header>
+        <div className='feed-content'>
+          <p className='feed-text b1reg'>{content}</p>
+        </div>
+        <div className='feed-count-container'>
+          <p className='count-text b3md'>좋아요 {likeCount}</p>
+          <span className='count-divider' />
+          <p className='count-text b3md'>댓글 {commentCount}</p>
+        </div>
+        <footer className='feed-footer'>
+          <div className='footer-left'>
+            <button className='icon-btn'>
+              <LikeIcon />
+            </button>
+            <button className='icon-btn'>
+              <ChatIcon />
+            </button>
+            <button className='icon-btn'>
+              <SharedIcon />
+            </button>
+          </div>
+          <div className='footer-right'>
+            <button className='icon-btn'>
+              <BookmarkIcon />
+            </button>
+          </div>
+        </footer>
+      </article>
+    </div>
+  );
+};
+
+export default Feed;
--- src/pages/feed-main/ui/FeedMainPage.tsx
+++ src/pages/feed-main/ui/FeedMainPage.tsx
@@ -1,9 +1,46 @@
+import Feed from '@/features/feed/ui/Feed';
 import { FeedMainHeader } from '@/features/feed-main-header/ui/FeedMainHeader';
+
+import './FeedMainPage.scss';
+
+const DUMMY_FEED = {
+  id: 1,
+  user: {
+    id: 1,
+    profileImage: 'https://picsum.photos/200/200',
+    name: '인생은 한 방',
+    content: 'bangdori',
+  },
+  title: 'Feed Title 1',
+  content: '오늘은 쇼핑을 엄청나게 해서 거지가 됐습니다',
+  images: [
+    {
+      id: 1,
+      imageUrl: 'https://picsum.photos/200/200',
+    },
+  ],
+
+  likeCount: 0,
+  commentCount: 0,
+
+  isLiked: false,
+  isBookmark: false,
+
+  createdAt: '2024-04-16 12:00:00',
+  updatedAt: '2024-04-16 12:00:00',
+};
 
 const FeedMainPage = () => {
   return (
     <section className='feed-main-section'>
       <FeedMainHeader />
+      <div className='feed-list'>
+        <Feed feed={DUMMY_FEED} />
+        <Feed feed={DUMMY_FEED} />
+        <Feed feed={DUMMY_FEED} />
+        <Feed feed={DUMMY_FEED} />
+        <Feed feed={DUMMY_FEED} />
+      </div>
     </section>
   );
 };
