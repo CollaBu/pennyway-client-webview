5874f56d79aa64b518a4afd9b6dcffced5f7abd6
BangDori <qudwns8865@naver.com>
2024-04-30 17:27:26 +0900

    feat: 피드 메인 페이지 헤더

--- src/app/routers/index.tsx
+++ src/app/routers/index.tsx
@@ -1,12 +1,14 @@
 import { createBrowserRouter, RouteObject } from 'react-router-dom';
 
+import FeedMainPage from '@/pages/feed-main/ui/FeedMainPage.tsx';
+
 import RootLayout from '../layout/RootLayout.tsx';
 
 const root: RouteObject[] = [
   {
     path: '/',
     element: <RootLayout />,
-    children: [{ index: true, element: <p>Hello World</p> }],
+    children: [{ index: true, element: <FeedMainPage /> }],
   },
 ];
 
---
+++ src/pages/feed-main/ui/FeedMainPage.tsx
@@ -0,0 +1,11 @@
+import { FeedMainHeader } from '@/features/feed-main-header/ui/FeedMainHeader';
+
+const FeedMainPage = () => {
+  return (
+    <section className='feed-main-section'>
+      <FeedMainHeader />
+    </section>
+  );
+};
+
+export default FeedMainPage;
