800d8ac7b10cc689edb8c56d7ecb0d03882ebb74
BangDori <qudwns8865@naver.com>
2024-05-03 00:00:06 +0900

    feat: 로딩 화면 스크롤 비활성화

--- src/widgets/feed-main-list/ui/SkeletonFeedMainList.tsx
+++ src/widgets/feed-main-list/ui/SkeletonFeedMainList.tsx
@@ -1,8 +1,9 @@
 import { SkeletonFeed } from './SkeletonFeed';
+import './SkeletonFeedMainList.scss';
 
 export const SkeletonFeedMainList = () => {
   return (
-    <section className='feed-list-section'>
+    <section className='skeleton-feed-section'>
       {new Array(5).fill(1).map((_, i) => (
         <SkeletonFeed key={i} />
       ))}
