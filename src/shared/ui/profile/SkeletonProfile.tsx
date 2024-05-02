576544690bcb236b56f5a2ef8681fa49b797c73a
BangDori <qudwns8865@naver.com>
2024-05-02 22:06:15 +0900

    feat: profile 스켈레톤 UI

---
+++ src/shared/ui/profile/SkeletonProfile.tsx
@@ -0,0 +1,10 @@
+import './SkeletonProfile.scss';
+
+export const SkeletonProfile = () => {
+  return (
+    <div className='skeleton-profile'>
+      <div className='skeleton-profile-image' />
+      <div className='skeleton-name-section' />
+    </div>
+  );
+};
