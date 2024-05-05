1696364d2a7e884a69a920a114c1164992d7302b
Uichan Lee <cksxkr5193@naver.com>
2024-05-05 23:11:49 +0900

    feat: profileFeed.d.ts에 ProfileFeed Interface 구현

--- src/shared/consts/types/index.ts
+++ src/shared/consts/types/index.ts
@@ -2,3 +2,4 @@ export type { Feed, Image } from './feed';
 export type { Comment } from './comment';
 export type { Like } from './like';
 export type { User } from './user';
+export type { ProfileFeed } from './profileFeed';
---
+++ src/shared/consts/types/profileFeed.d.ts
@@ -0,0 +1,10 @@
+export interface ProfileFeed {
+  id: number;
+  profileImage: string;
+  title: string;
+  content: string;
+
+  likeCount: number;
+  commentCount: number;
+}
+혀ㅑ;
