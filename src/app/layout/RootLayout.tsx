394091b931ca2e8ea93ed963b8203789e6531d58
BangDori <qudwns8865@naver.com>
2024-05-08 20:18:09 +0900

    feat: iPhone Layout 위치 수정

--- src/app/layout/RootLayout.tsx
+++ src/app/layout/RootLayout.tsx
@@ -1,10 +1,5 @@
-import { IPhoneLayout } from './iPhone';
+import { Outlet } from 'react-router-dom';
 
-/**
- * 🚨 현재 iPhone Layout은 DEV 모드에서만 활성화되는 부분이지만,
- * 실제 PROD 환경에서도 볼 수 있도록 하기 위해 따로 처리하지 않겠습니다.
- * 하지만 실제 PROD 모드로 배포할 경우에는 <IPhoneLayout />을 제거하고 <Outlet />으로 변경해주세요.
- */
 export default function RootLayout() {
-  return <IPhoneLayout />;
+  return <Outlet />;
 }
--- src/app/routers/index.tsx
+++ src/app/routers/index.tsx
@@ -2,12 +2,18 @@ import { createBrowserRouter, RouteObject } from 'react-router-dom';
 
 import { FeedMainPage } from '@/pages/index.ts';
 
-import RootLayout from '../layout/RootLayout.tsx';
+import { IPhoneLayout } from '../layout/iPhone/index.ts';
+// import RootLayout from '../layout/RootLayout.tsx';
 
+/**
+ * 🚨 현재 iPhone Layout은 DEV 모드에서만 활성화되는 부분이지만,
+ * 실제 PROD 환경에서도 볼 수 있도록 하기 위해 따로 처리하지 않겠습니다.
+ * 하지만 실제 PROD 모드로 배포할 경우에는 <IPhoneLayout />을 제거하고 <RootLayout />으로 변경해주세요.
+ */
 const root: RouteObject[] = [
   {
     path: '/',
-    element: <RootLayout />,
+    element: <IPhoneLayout />,
     children: [{ index: true, element: <FeedMainPage /> }],
   },
 ];
