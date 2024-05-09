8620eeb09509ee7b9f761811e70bc21660ede3d1
BangDori <qudwns8865@naver.com>
2024-05-10 01:06:20 +0900

    style: 주석 수정

--- src/features/feed-main-like/test/useLike.test.tsx
+++ src/features/feed-main-like/test/useLike.test.tsx
@@ -8,11 +8,13 @@ import { useLike } from '../api';
 
 test('좋아요 상태가 아닐 때, 좋아요 버튼을 클릭하면 좋아요 요청이 발생한다.', async () => {
   // given
+  // requestLikeFeed 함수를 스파이한다.
   const spy = vi.spyOn(likeModule, 'requestLikeFeed');
   const { result } = renderHook(() => useLike(1, false), {
     wrapper: createQueryClientWrapper(),
   });
 
+  // requestLikeFeed가 호출되지 않았는지 확인
   await waitFor(() => expect(spy).not.toHaveBeenCalled());
 
   // when
@@ -34,10 +36,8 @@ test('좋아요 상태일 때, 좋아요 버튼을 클릭하면 좋아요 취소
   await waitFor(() => expect(spy).not.toHaveBeenCalled());
 
   // when
-  // 좋아요 버튼 클릭
   await act(async () => result.current.handleLikeFeed());
 
   // then
-  // requestUnlikeFeed가 호출되었는지 확인
   await waitFor(() => expect(spy).toHaveBeenCalled());
 });
