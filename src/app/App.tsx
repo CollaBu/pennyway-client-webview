090bdf08c3f3ef39864ca4e14cd78f5c84b5ec70
BangDori <qudwns8865@naver.com>
2024-05-10 20:50:09 +0900

    refactor: 네트워크 에러 토스트 최상단 위치

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,6 +3,7 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from 'react-router-dom';
 
 import { queryClient } from '@/shared/react-query';
+import { NetworkErrorToast } from '@/shared/ui';
 
 import { router } from './routers/index';
 import './styles/global.scss';
@@ -11,6 +12,8 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+
+      <NetworkErrorToast />
       <ReactQueryDevtools />
     </QueryClientProvider>
   );
