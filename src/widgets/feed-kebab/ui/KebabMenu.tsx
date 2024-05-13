dfc2b4d06aa811409bb2ae51262269020521f0ec
BangDori <qudwns8865@naver.com>
2024-05-14 00:11:03 +0900

    feat: 피드 케밥 메뉴 UI

--- src/widgets/feed-kebab/ui/KebabMenu.tsx
+++ src/widgets/feed-kebab/ui/KebabMenu.tsx
@@ -1,3 +1,14 @@
+import './KebabMenu.scss';
+
 export const KebabMenu = () => {
-  return <div>KebabMenu</div>;
+  return (
+    <ul className='kebab-menu-list'>
+      <li className='kebab-menu-item'>
+        <button className='item-btn b2md'>게시물 숨기기</button>
+      </li>
+      <li className='kebab-menu-item'>
+        <button className='item-btn b2md'>신고하기</button>
+      </li>
+    </ul>
+  );
 };
