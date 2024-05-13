4ff328a0674d26a4e4fe51d267bd09e2063cd99f
BangDori <qudwns8865@naver.com>
2024-05-14 00:39:08 +0900

    style: useToggle 주석 추가

--- src/shared/hooks/useToggle.tsx
+++ src/shared/hooks/useToggle.tsx
@@ -1,6 +1,12 @@
 import { useCallback, useState } from 'react';
 import type { Dispatch, SetStateAction } from 'react';
 
+/**
+ * reference: https://usehooks-ts.com/react-hook/use-toggle
+ * useToggle 훅은 boolean 값과 값을 토글할 수 있는 함수를 반환하는 커스텀 훅입니다.
+ * @param defaultValue 초기값
+ * @returns [상태값, 토글 함수, 상태 설정 함수]
+ */
 export function useToggle(
   defaultValue?: boolean,
 ): [boolean, () => void, Dispatch<SetStateAction<boolean>>] {
