0153056de67e7b54b50d0b8e62af8a42b2f8a576
BangDori <qudwns8865@naver.com>
2024-05-13 18:53:12 +0900

    feat: 아이폰 회전 기능 추가

--- src/app/layout/iPhone/hooks/useUtilityIPhone.tsx
+++ src/app/layout/iPhone/hooks/useUtilityIPhone.tsx
@@ -1,21 +1,30 @@
 import { useEffect, useRef } from 'react';
 
 const IPHONE_MIN_SIZE = 60;
-const IPHONE_INIT_SIZE = 60;
+const IPHONE_INIT_SIZE = 80;
 const IPHONE_MAX_SIZE = 100;
+
+const IPHONE_PROTRAIT_MODE = 0; // 세로 모드
+const IPHONE_LANDSCAPE_MODE = -90; // 가로 모드
 
 /**
  * iPhone 레이아웃의 크기를 동적으로 변경하는 훅입니다.
  * @returns iPhone 레이아웃의 참조와 사이즈 변경 핸들러
  */
-export const useDynamicSize = () => {
+export const useUtilityIPhone = () => {
   const iPhoneSizeRef = useRef<number>(IPHONE_INIT_SIZE);
+  const iPhoneModeRef = useRef<number>(IPHONE_PROTRAIT_MODE);
   const iPhoneLayoutRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     document.documentElement.style.setProperty(
       '--iphone-size',
       `${IPHONE_INIT_SIZE}%`,
+    );
+
+    document.documentElement.style.setProperty(
+      '--iphone-mode',
+      `${IPHONE_PROTRAIT_MODE}deg`,
     );
   }, []);
 
@@ -43,5 +52,23 @@ export const useDynamicSize = () => {
     changeStyle();
   };
 
-  return { iPhoneLayoutRef, handleSizeUp, handleSizeDown };
+  const handleRotate = () => {
+    if (iPhoneModeRef.current === IPHONE_PROTRAIT_MODE) {
+      // 가로 모드로 변경
+      iPhoneModeRef.current = IPHONE_LANDSCAPE_MODE;
+      document.documentElement.style.setProperty(
+        '--iphone-mode',
+        `${IPHONE_LANDSCAPE_MODE}deg`,
+      );
+    } else {
+      // 세로 모드로 변경
+      iPhoneModeRef.current = IPHONE_PROTRAIT_MODE;
+      document.documentElement.style.setProperty(
+        '--iphone-mode',
+        `${IPHONE_PROTRAIT_MODE}deg`,
+      );
+    }
+  };
+
+  return { iPhoneLayoutRef, handleRotate, handleSizeUp, handleSizeDown };
 };
