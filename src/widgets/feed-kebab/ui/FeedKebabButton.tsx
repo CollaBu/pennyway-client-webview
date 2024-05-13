b3fe295bf0aebaa3bc86ec98924d60cba52de2d6
BangDori <qudwns8865@naver.com>
2024-05-13 23:55:04 +0900

    feat: 케밥 메뉴 상호작용 기능 연결

--- src/widgets/feed-kebab/ui/FeedKebabButton.tsx
+++ src/widgets/feed-kebab/ui/FeedKebabButton.tsx
@@ -1,9 +1,18 @@
 import { Icon } from '@/shared/ui';
 
+import { useVisibilityKebabMenu } from '../model';
+
+import { KebabMenu } from './KebabMenu';
+
 export const FeedKebabButton = () => {
+  const { isVisibilityKebabMenu, toggleVisibility } = useVisibilityKebabMenu();
+
   return (
-    <button className='icon kebab-icon-btn'>
-      <Icon name='kebab-menu' width='20' height='20' />
-    </button>
+    <>
+      <button className='icon kebab-icon-btn' onClick={toggleVisibility}>
+        <Icon name='kebab-menu' width='20' height='20' />
+      </button>
+      {isVisibilityKebabMenu && <KebabMenu />}
+    </>
   );
 };
---
+++ src/widgets/feed-kebab/ui/KebabMenu.tsx
@@ -0,0 +1,3 @@
+export const KebabMenu = () => {
+  return <div>KebabMenu</div>;
+};
