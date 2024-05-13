185d9c2f0fc1720e291aeb3fc6a4d8b216c19e51
BangDori <qudwns8865@naver.com>
2024-05-13 23:51:14 +0900

    feat: 피드 케밥 메뉴 아이콘 분리

---
+++ src/widgets/feed-kebab/ui/FeedKebabButton.tsx
@@ -0,0 +1,9 @@
+import { Icon } from '@/shared/ui';
+
+export const FeedKebabButton = () => {
+  return (
+    <button className='icon kebab-icon-btn'>
+      <Icon name='kebab-menu' width='20' height='20' />
+    </button>
+  );
+};
