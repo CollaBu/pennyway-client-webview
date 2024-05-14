1cac1f41f9971ac60be823f985a0ce27a82306e9
BangDori <qudwns8865@naver.com>
2024-05-14 15:14:31 +0900

    feat: 체크박스 숨기기 UI

--- src/features/feed-reports/ui/FeedReportsForm.tsx
+++ src/features/feed-reports/ui/FeedReportsForm.tsx
@@ -1,3 +1,4 @@
+import { useToggle } from '@/shared/hooks';
 import { Icon } from '@/shared/ui';
 
 import { MAX_REPORT_CONTENT_LENGTH, REPORTS } from '../consts';
@@ -15,6 +16,7 @@ export const FeedReportsForm: React.FC<FeedReportsFormProps> = ({
 }) => {
   const { checkedItemMap, handleClickItem } = useCheckbox();
   const { content, contentLength, handleInputContent } = useInputReports();
+  const [isHiddenPost, setIsHiddenost] = useToggle(false);
 
   return (
     <ConfirmReportModal
@@ -32,7 +34,11 @@ export const FeedReportsForm: React.FC<FeedReportsFormProps> = ({
                 onClick={() => handleClickItem(item.id)}
               >
                 <Icon
-                  name={checkedItemMap.get(item.id) ? 'checkon' : 'checkoff'}
+                  name={
+                    checkedItemMap.get(item.id)
+                      ? 'checkbox-circle_on'
+                      : 'checkbox-circle_off'
+                  }
                   width='20'
                   height='20'
                 />
@@ -51,6 +57,21 @@ export const FeedReportsForm: React.FC<FeedReportsFormProps> = ({
             maxLength={MAX_REPORT_CONTENT_LENGTH}
           />
           <span className='textarea-text-count b2md'>{contentLength}/100</span>
+        </div>
+
+        <div className='hide-checkbox-container'>
+          <button
+            className='checkbox-btn'
+            type='button'
+            onClick={setIsHiddenost}
+          >
+            <Icon
+              name={isHiddenPost ? 'checkbox-square_on' : 'checkbox-square_off'}
+              width='20'
+              height='20'
+            />
+          </button>
+          <p className='hide-checkbox-text b1md'>해당 게시물 숨기기</p>
         </div>
       </>
     </ConfirmReportModal>
