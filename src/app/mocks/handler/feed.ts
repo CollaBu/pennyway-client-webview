a2a238c7557b4723415868feb0145b82448e8935
BangDori <qudwns8865@naver.com>
2024-05-17 02:19:38 +0900

    feat: 피드 숨기기 API 분리

--- src/app/mocks/handler/feed.ts
+++ src/app/mocks/handler/feed.ts
@@ -189,22 +189,4 @@ export const feedHandlers = [
 
     return createHttpSuccessResponse({});
   }),
-
-  // 7️⃣ 피드 숨기기
-  http.put('/feeds/:feed_id/hides', ({ params }) => {
-    const { feed_id } = params;
-
-    if (isNaN(Number(feed_id))) {
-      return createHttpErrorResponse('4220');
-    }
-
-    const formattedFeedId = Number(feed_id);
-
-    if (!feeds[formattedFeedId]) {
-      return createHttpErrorResponse('4040');
-    }
-
-    hiddens[formattedFeedId] = true;
-    return createHttpSuccessResponse({ isHidden: true });
-  }),
 ];
