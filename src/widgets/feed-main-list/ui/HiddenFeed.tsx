765dbf8620ed3c1dc3f136e7e8879b86f42f7a93
BangDori <qudwns8865@naver.com>
2024-05-16 22:10:43 +0900

    feat: 구분선 표시

--- src/widgets/feed-main-list/ui/HiddenFeed.tsx
+++ src/widgets/feed-main-list/ui/HiddenFeed.tsx
@@ -6,9 +6,11 @@ import './HiddenFeed.scss';
 const HiddenFeed: React.FC<{ message: string }> = ({ message }) => {
   return (
     <div className='feed-hidden-wrapper'>
-      <Icon name='check_mint' width='24' height='24' />
-      <p className='hidden-reason-msg b2md'>{message}</p>
-      <button className='hidden-cancel-btn b2md'>취소</button>
+      <div className='feed-hidden-container'>
+        <Icon name='check_mint' width='24' height='24' />
+        <p className='hidden-reason-msg b2md'>{message}</p>
+        <button className='hidden-cancel-btn b2md'>취소</button>
+      </div>
     </div>
   );
 };
