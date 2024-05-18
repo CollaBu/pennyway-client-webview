738e30fc5445a50b71f5a6ee5d8abe0e13aaf999
BangDori <qudwns8865@naver.com>
2024-05-18 18:12:35 +0900

    feat: React Query 적용

--- src/shared/tests/setup.tsx
+++ src/shared/tests/setup.tsx
@@ -1,24 +1,13 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { render as testRender } from '@testing-library/react';
 import { PropsWithChildren, ReactElement } from 'react';
 import { MemoryRouter } from 'react-router-dom';
 
-const generateQueryClient = () => {
-  const queryClientOptions = {
-    defaultOptions: {
-      queries: {
-        retry: false,
-      },
-    },
-  };
-
-  return new QueryClient(queryClientOptions);
-};
+import { queryClient } from '../react-query';
 
 // hooks를 사용하는 컴포넌트를 테스트할 때 필요한 wrapper를 생성하는 함수
 // from https://tkdodo.eu/blog/testing-react-query#for-custom-hooks
 export const createQueryClientWrapper = () => {
-  const queryClient = generateQueryClient();
   return ({ children }: PropsWithChildren) => (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
@@ -27,8 +16,6 @@ export const createQueryClientWrapper = () => {
 // custom render 함수
 // reference: https://testing-library.com/docs/react-testing-library/setup#custom-render
 const customRender = (children: ReactElement, baseEntries?: string[]) => {
-  const queryClient = generateQueryClient();
-
   return testRender(
     <QueryClientProvider client={queryClient}>
       <MemoryRouter initialEntries={baseEntries}>{children}</MemoryRouter>
