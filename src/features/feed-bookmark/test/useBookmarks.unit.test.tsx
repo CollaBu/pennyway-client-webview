342a8d7d6d4fc185c1796fe904c16f76e1efeb02
BangDori <qudwns8865@naver.com>
2024-05-18 20:01:50 +0900

    feat: 북마크 단위 테스트 리팩토링

--- src/features/feed-bookmark/test/useBookmarks.unit.test.tsx
+++ src/features/feed-bookmark/test/useBookmarks.unit.test.tsx
@@ -1,58 +1,40 @@
 import { renderHook, act, waitFor } from '@testing-library/react';
-import { describe, expect, it, vi } from 'vitest';
+import { describe, expect, it } from 'vitest';
 
-import * as bookmarkModule from '@/shared/axios';
 import { createQueryClientWrapper } from '@/shared/tests';
 
 import { useBookmarks } from '../api';
 
-describe('북마크 상태가 아닐 때 북마크 버튼을 클릭하면', () => {
-  it('북마크 요청이 발생한다.', async () => {
-    // given
-    const spy = vi.spyOn(bookmarkModule, 'requestBookmarkFeed');
+describe('북마크 기능 테스트', () => {
+  it('북마크 상태가 아닐 때, 북마크 버튼을 클릭하면 북마크 된다', async () => {
     const { result } = renderHook(() => useBookmarks(1, false), {
       wrapper: createQueryClientWrapper(),
     });
 
-    // when
-    expect(spy).not.toHaveBeenCalled();
     act(() => result.current.handleBookmarkFeed());
 
     // then
-    await waitFor(() => expect(spy).toHaveBeenCalled());
+    await waitFor(() => {
+      const {
+        data: { isBookmarked },
+      } = result.current.data;
+      expect(isBookmarked).toBeTruthy();
+    });
   });
 
-  it('북마크 상태가 변경된다.', async () => {
-    // given
-    const {
-      data: { isBookmarked },
-    } = await bookmarkModule.requestBookmarkFeed(1);
-
-    expect(isBookmarked).toBeTruthy();
-  });
-});
-
-describe('북마크 상태일 때 북마크 버튼을 클릭하면', () => {
-  it('북마크 취소 요청이 발생한다.', async () => {
-    // given
-    const spy = vi.spyOn(bookmarkModule, 'requestUnbookmarkFeed');
+  it('북마크 상태일 때, 북마크 버튼을 클릭하면 북마크가 취소된다', async () => {
     const { result } = renderHook(() => useBookmarks(1, true), {
       wrapper: createQueryClientWrapper(),
     });
 
-    // when
-    expect(spy).not.toHaveBeenCalled();
     act(() => result.current.handleBookmarkFeed());
 
     // then
-    await waitFor(() => expect(spy).toHaveBeenCalled());
-  });
-
-  it('북마크 상태가 변경된다.', async () => {
-    const {
-      data: { isBookmarked },
-    } = await bookmarkModule.requestUnbookmarkFeed(1);
-
-    expect(isBookmarked).toBeFalsy();
+    await waitFor(() => {
+      const {
+        data: { isBookmarked },
+      } = result.current.data;
+      expect(isBookmarked).toBeFalsy();
+    });
   });
 });
