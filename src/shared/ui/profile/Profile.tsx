33e71d2a55a91967917d783e1cd0408562de65bd
Legitgoons <cksxkr5193@naver.com>
2024-05-23 18:46:21 +0900

    feat: profile 임시 링크 추가

--- src/shared/ui/profile/Profile.tsx
+++ src/shared/ui/profile/Profile.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import { Icon } from '..';
 import './Profile.scss';
 
@@ -7,20 +9,26 @@ interface ProfileProps {
   content: string;
 }
 
+/**
+ * @todo 임시로 구현된 Link, 추후 적절한 user로 이동하도록 수정
+ */
+
 export const Profile = ({ profileImage, name, content }: ProfileProps) => {
   return (
     <div className='profile'>
-      {profileImage ? (
-        <img
-          className='profile-image'
-          src={profileImage}
-          alt={`${name} profile image`}
-        />
-      ) : (
-        <div className='no-proile-background'>
-          <Icon name='no-profile' width='32' height='32' />
-        </div>
-      )}
+      <Link to={'/user'}>
+        {profileImage ? (
+          <img
+            className='profile-image'
+            src={profileImage}
+            alt={`${name} profile image`}
+          />
+        ) : (
+          <div className='no-proile-background'>
+            <Icon name='no-profile' width='32' height='32' />
+          </div>
+        )}
+      </Link>
       <div className='name-section'>
         <h5 className='name b1semi'>{name}</h5>
         <p className='content b3md'>{content}</p>
