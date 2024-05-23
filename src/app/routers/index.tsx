6f02c9cda6a2dea056957f98cf882390fef4a8d3
Legitgoons <cksxkr5193@naver.com>
2024-05-23 23:42:14 +0900

    feat: router index.ts 수정

--- src/app/routers/index.tsx
+++ src/app/routers/index.tsx
@@ -16,7 +16,7 @@ const root: RouteObject[] = [
     element: <IPhoneLayout />,
     children: [
       { index: true, element: <FeedMainPage /> },
-      { path: '/user', element: <ProfileMainPage /> },
+      { path: 'user', element: <ProfileMainPage /> },
     ],
   },
 ];
