36ce49dc5cfe51b80a5a0577145aa382568e96e5
Legitgoons <cksxkr5193@naver.com>
2024-05-24 14:21:23 +0900

    feat: ProfileMainPage 수정

--- src/pages/profile/ui/ProfileMainPage.tsx
+++ src/pages/profile/ui/ProfileMainPage.tsx
@@ -1,12 +1,33 @@
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
 import { ProfileFeedList } from '@/widgets/profile-feed-list';
-import { ProfileHeader } from '@/widgets/profile-header';
 import { ProfileUser } from '@/widgets/profile-user';
 
+/**
+ * @todo  userPK값 가져와서 본인인지 판별하도록 수정
+ */
+
 export const ProfileMainPage = () => {
+  const { userId } = useParams<{ userId: string }>();
+  const formattedUserId = Number(userId);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  if (formattedUserId === 1) {
+    return (
+      <main>
+        <ProfileUser userId={formattedUserId} isOwner />
+        <ProfileFeedList />
+      </main>
+    );
+  }
+
   return (
     <main>
-      <ProfileHeader name='2weeksone' />
-      <ProfileUser />
+      <ProfileUser userId={formattedUserId} isOwner={false} />
       <ProfileFeedList />
     </main>
   );
