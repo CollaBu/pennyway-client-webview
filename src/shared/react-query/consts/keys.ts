3efc682296f34eaaf320db086e0d176432b0a7da
Legitgoons <cksxkr5193@naver.com>
2024-05-24 14:16:46 +0900

    feat: useGetUser hook 구현

--- src/shared/react-query/consts/keys.ts
+++ src/shared/react-query/consts/keys.ts
@@ -1,3 +1,4 @@
 export const QUERY_KEYS = Object.freeze({
   feeds: 'feeds',
+  users: 'users',
 });
---
+++ src/widgets/profile-user/api/useGetUser.ts
@@ -0,0 +1,30 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { axiosInstance } from '@/shared/axios';
+import { FetchUser } from '@/shared/consts';
+import { QUERY_KEYS } from '@/shared/react-query';
+
+async function fetchUser(userId: number): Promise<FetchUser> {
+  const { data } = await axiosInstance.get(`/users/${userId}`);
+  return data;
+}
+
+export const useGetUser = (userId: number) => {
+  const {
+    data,
+    isLoading,
+    isError,
+    refetch: refetchUser,
+  } = useQuery({
+    queryKey: [QUERY_KEYS.users, userId],
+    queryFn: () => fetchUser(userId),
+    enabled: !!userId,
+  });
+
+  return {
+    data,
+    isLoading,
+    isError,
+    refetchUser,
+  };
+};
