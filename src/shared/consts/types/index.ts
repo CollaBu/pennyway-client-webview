6289ee5c0565057d458008439a3b4dbeaf99c6f4
Legitgoons <cksxkr5193@naver.com>
2024-05-26 19:47:28 +0900

    fix: shared/consts/types/index.ts 수정
    잘못된 태스크 분리로 인해 잘못 삭제한 export ProfileFeed 복구

--- src/shared/consts/types/index.ts
+++ src/shared/consts/types/index.ts
@@ -2,3 +2,4 @@ export type * from './feed';
 export type { Comment } from './comment';
 export type { Like } from './like';
 export type * from './user';
+export type { ProfileFeed } from './profileFeed';
