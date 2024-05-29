370067633289222d630a2f156073b8228745d55b
Uichan Lee <cksxkr5193@naver.com>
2024-05-29 21:25:34 +0900

    feat: ScrollRestoration RootLayout으로 이동

--- src/app/routers/index.tsx
+++ src/app/routers/index.tsx
@@ -1,8 +1,4 @@
-import {
-  createBrowserRouter,
-  RouteObject,
-  ScrollRestoration,
-} from 'react-router-dom';
+import { createBrowserRouter, RouteObject } from 'react-router-dom';
 
 import { FeedMainPage } from '@/pages/feed-main';
 import { ProfileMainPage } from '@/pages/profile';
@@ -12,16 +8,7 @@ import { RootLayout } from '../layout';
 const root: RouteObject[] = [
   {
     path: '/',
-    element: (
-      <>
-        <RootLayout />
-        <ScrollRestoration
-          getKey={(location) => {
-            return location.pathname;
-          }}
-        />
-      </>
-    ),
+    element: <RootLayout />,
     children: [
       { index: true, element: <FeedMainPage /> },
       { path: 'users/:userId', element: <ProfileMainPage /> },
