0a633c35d1d1d100d2435a061c3b5bd6d48e77a1
Uichan Lee <cksxkr5193@naver.com>
2024-05-29 22:19:44 +0900

    feat: FeedMainPage closeKebab 추가
    useEffect의 클린업 기능을 사용해, FeedMainPage가 언마운트 될 시 kebab메뉴가 닫히도록 했습니다.

--- src/pages/feed-main/ui/FeedMainPage.tsx
+++ src/pages/feed-main/ui/FeedMainPage.tsx
@@ -1,8 +1,17 @@
+import { useEffect } from 'react';
+
+import { useFeedKebabStore } from '@/widgets/feed-kebab';
 import { FeedMainHeader } from '@/widgets/feed-main-header';
 import { FeedMainList } from '@/widgets/feed-main-list';
 import './FeedMainPage.scss';
 
 export const FeedMainPage = () => {
+  useEffect(() => {
+    return () => {
+      useFeedKebabStore.getState().closeKebab();
+    };
+  }, []);
+
   return (
     <main className='feed-main-page'>
       <FeedMainHeader />
