a2b8f832251be0e4c6963859cbf57e5c7a2abdc4
BangDori <qudwns8865@naver.com>
2024-05-29 20:39:34 +0900

    feat: 아이폰 레이아웃

--- src/app/layout/RootLayout.tsx
+++ src/app/layout/RootLayout.tsx
@@ -1,10 +1,15 @@
+import { IPhoneLayout } from 'react-iphone-layout';
+import 'react-iphone-layout/dist/ReactIPhoneLayout.css';
 import { Outlet } from 'react-router-dom';
+
 import './RootLayout.scss';
 
 export const RootLayout = () => {
   return (
-    <div className='wrap'>
-      <Outlet />
-    </div>
+    <IPhoneLayout>
+      <div className='wrap'>
+        <Outlet />
+      </div>
+    </IPhoneLayout>
   );
 };
