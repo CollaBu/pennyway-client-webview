32c52dd4c18343453d998906b71be98913822e74
Uichan Lee <cksxkr5193@naver.com>
2024-05-29 21:18:02 +0900

    fix: profile scroll 이슈 해결
    router/index.ts에 ScrollRestoration를 추가하였습니다.

--- src/app/routers/index.tsx
+++ src/app/routers/index.tsx
@@ -1,4 +1,8 @@
-import { createBrowserRouter, RouteObject } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  RouteObject,
+  ScrollRestoration,
+} from 'react-router-dom';
 
 import { FeedMainPage } from '@/pages/feed-main';
 import { ProfileMainPage } from '@/pages/profile';
@@ -8,7 +12,16 @@ import { RootLayout } from '../layout';
 const root: RouteObject[] = [
   {
     path: '/',
-    element: <RootLayout />,
+    element: (
+      <>
+        <RootLayout />
+        <ScrollRestoration
+          getKey={(location) => {
+            return location.pathname;
+          }}
+        />
+      </>
+    ),
     children: [
       { index: true, element: <FeedMainPage /> },
       { path: 'users/:userId', element: <ProfileMainPage /> },
