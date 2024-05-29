839f1376c55ddacaaca21304a4ed42a8d79f37ec
BangDori <qudwns8865@naver.com>
2024-05-29 20:42:14 +0900

    style: 주석 위치 수정

--- src/app/layout/RootLayout.tsx
+++ src/app/layout/RootLayout.tsx
@@ -4,6 +4,11 @@ import { Outlet } from 'react-router-dom';
 
 import './RootLayout.scss';
 
+/**
+ * 🚨 현재 적용되어 있는 IPhoneLayout은 DEV 모드에서만 활성화되어야 하지만,
+ * 실제 개발 서버에서도 볼 수 있도록 하기 위해 따로 처리하지 않겠습니다.
+ * 하지만 실제 PROD 모드로 배포할 경우에는 <IPhoneLayout />을 제거해주세요.
+ */
 export const RootLayout = () => {
   return (
     <IPhoneLayout>
--- src/app/routers/index.tsx
+++ src/app/routers/index.tsx
@@ -5,11 +5,6 @@ import { ProfileMainPage } from '@/pages/profile';
 
 import { RootLayout } from '../layout';
 
-/**
- * 🚨 현재 iPhone Layout은 DEV 모드에서만 활성화되는 부분이지만,
- * 실제 PROD 환경에서도 볼 수 있도록 하기 위해 따로 처리하지 않겠습니다.
- * 하지만 실제 PROD 모드로 배포할 경우에는 <IPhoneLayout />을 제거하고 <RootLayout />으로 변경해주세요.
- */
 const root: RouteObject[] = [
   {
     path: '/',
