56cae1d10279c3e91407669a145e8242b3750881
Uichan Lee <cksxkr5193@naver.com>
2024-06-26 14:26:08 +0900

    feat: follow 관련 unit test 수정
    테스트 값을 유효한 값으로 변경하고, 구조분해할당을 수정해 적절한 값으로 expect를 진행하도록 했습니다.
    또한 유효하지 않은 test를 제거하고, 문구를 더 명확하게 했습니다.

--- src/features/follow/test/useFollow.unit.test.tsx
+++ src/features/follow/test/useFollow.unit.test.tsx
@@ -6,57 +6,40 @@ import { createQueryClientWrapper } from '@/shared/tests';
 import { useFollow } from '../api';
 
 describe('Follow 기능 테스트', () => {
-  it('self 상태일 때 Follow 기능이 동작하지 않는다.', async () => {
-    // given
-    const { result } = renderHook(() => useFollow('self', false), {
-      wrapper: createQueryClientWrapper(),
+  describe('none 상태일 때', () => {
+    it('공개 계정이라면 following 상태로 변경된다.', async () => {
+      //given
+      const { result } = renderHook(() => useFollow(9, false), {
+        wrapper: createQueryClientWrapper(),
+      });
+
+      //when
+      act(() => result.current.handleFollow());
+
+      //then
+      await waitFor(() => {
+        const {
+          data: { relationshipStatus: initialStatus },
+        } = result.current.data;
+
+        expect(initialStatus).toBe('following');
+      });
     });
 
-    // when
-    act(() => result.current.handleFollow());
+    it('비공개 계정이라면 pending 상태로 변경된다.', async () => {
+      const { result } = renderHook(() => useFollow(4, true), {
+        wrapper: createQueryClientWrapper(),
+      });
 
-    // then
-    await waitFor(() => {
-      const {
-        data: { initialStatus },
-      } = result.current.data;
+      act(() => result.current.handleFollow());
 
-      expect(initialStatus).toBe('self');
-    });
-  });
+      await waitFor(() => {
+        const {
+          data: { relationshipStatus: initialStatus },
+        } = result.current.data;
 
-  it('none 상태일 때 Follow 기능 실행 시, 공개 계정이라면 following 상태로 변경된다.', async () => {
-    //given
-    const { result } = renderHook(() => useFollow('none', false), {
-      wrapper: createQueryClientWrapper(),
-    });
-
-    //when
-    act(() => result.current.handleFollow());
-
-    //then
-    await waitFor(() => {
-      const {
-        data: { initialStatus },
-      } = result.current.data;
-
-      expect(initialStatus).toBe('following');
-    });
-  });
-
-  it('none 상태일 때 Follow 기능 실행 시, 비공개 계정이라면 pending 상태로 변경된다.', async () => {
-    const { result } = renderHook(() => useFollow('none', true), {
-      wrapper: createQueryClientWrapper(),
-    });
-
-    act(() => result.current.handleFollow());
-
-    await waitFor(() => {
-      const {
-        data: { initialStatus },
-      } = result.current.data;
-
-      expect(initialStatus).toBe('pending');
+        expect(initialStatus).toBe('pending');
+      });
     });
   });
 });
--- src/features/follow/test/useUnfollow.unit.test.tsx
+++ src/features/follow/test/useUnfollow.unit.test.tsx
@@ -6,9 +6,9 @@ import { createQueryClientWrapper } from '@/shared/tests';
 import { useUnfollow } from '../api';
 
 describe('Unfollow 기능 테스트', () => {
-  it('self 상태일 때 Unfollow 기능이 동작하지 않는다.', async () => {
+  it('following 상태일 때, none 상태로 변경된다.', async () => {
     // given
-    const { result } = renderHook(() => useUnfollow('self', false), {
+    const { result } = renderHook(() => useUnfollow(2, false), {
       wrapper: createQueryClientWrapper(),
     });
 
@@ -18,35 +18,16 @@ describe('Unfollow 기능 테스트', () => {
     // then
     await waitFor(() => {
       const {
-        data: { initialStatus },
-      } = result.current.data;
-
-      expect(initialStatus).toBe('self');
-    });
-  });
-
-  it('following 상태일 때 Unfollow 기능 실행 시, none 상태로 변경된다.', async () => {
-    // given
-    const { result } = renderHook(() => useUnfollow('following', false), {
-      wrapper: createQueryClientWrapper(),
-    });
-
-    // when
-    act(() => result.current.handleUnfollow());
-
-    // then
-    await waitFor(() => {
-      const {
-        data: { initialStatus },
+        data: { relationshipStatus: initialStatus },
       } = result.current.data;
 
       expect(initialStatus).toBe('none');
     });
   });
 
-  it('pending 상태일 때 Unfollow 기능 실행 시, none 상태로 변경된다.', async () => {
+  it('pending 상태일 때, none 상태로 변경된다.', async () => {
     // given
-    const { result } = renderHook(() => useUnfollow('pending', true), {
+    const { result } = renderHook(() => useUnfollow(3, true), {
       wrapper: createQueryClientWrapper(),
     });
 
@@ -56,7 +37,7 @@ describe('Unfollow 기능 테스트', () => {
     //then
     await waitFor(() => {
       const {
-        data: { initialStatus },
+        data: { relationshipStatus: initialStatus },
       } = result.current.data;
 
       expect(initialStatus).toBe('none');
