ae66f541e2d7c069326363a621eaebf789cc3bc9
Legitgoons <cksxkr5193@naver.com>
2024-06-26 23:43:49 +0900

    feat: handleMutationError에 mutationKeys 배열 생성, follow 추가

--- src/shared/react-query/dir/handleQuery.ts
+++ src/shared/react-query/dir/handleQuery.ts
@@ -40,7 +40,8 @@ export function handleMutationError(
   mutation: Mutation<unknown, unknown, unknown, unknown>,
 ) {
   const { options } = mutation;
+  const mutationKeys = ['feed-report', 'follow'];
 
-  if (options.mutationKey?.includes('feed-report'))
+  if (mutationKeys.some((key) => options.mutationKey?.includes(key)))
     showToastHandler('caution', '다시 시도해 주세요');
 }
