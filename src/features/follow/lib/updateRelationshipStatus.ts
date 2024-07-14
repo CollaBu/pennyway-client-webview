e1aec2927c1573d4c9dfe53f23cd07734e69bf4e
Uichan Lee <cksxkr5193@naver.com>
2024-07-14 18:13:54 +0900

    feat: calculateRelationshipStatus 함수 이름 수정
    RelationshipStatusCalculate => calculateRelationshipStatus로 이름 변경

--- src/features/follow/lib/updateRelationshipStatus.ts
+++ src/features/follow/lib/updateRelationshipStatus.ts
@@ -1,12 +1,13 @@
 import { RelationshipStatus, FetchRelationshipStatus } from '@/shared/consts';
 
 /**
- * 팔로우, 언팔로우/팔로우 취소에 따라 변경된 관계 상태를 계산하는 함수
- * @param relationshipStatus 이전 관계 상태
- * @param locked 비공개 계정 여부
+ * 팔로우, 언팔로우/팔로우 취소에 따라 변경되는 관계 상태를 계산하는 함수
+ * @param {RelationshipStatus} relationshipStatus 이전 관계 상태
+ * @param {boolean} locked 비공개 계정 여부
+ * @returns {RelationshipStatus} 변경된 관계 상태
  */
 
-const RelationshipStatusCalculate = (
+const calculateRelationshipStatus = (
   relationshipStatus: RelationshipStatus,
   locked: boolean,
 ) => {
@@ -31,7 +32,7 @@ export function updateRelationshipStatus(
   return {
     code: previousRelationshipStatusData.code,
     data: {
-      relationshipStatus: RelationshipStatusCalculate(
+      relationshipStatus: calculateRelationshipStatus(
         previousRelationshipStatusData.data.relationshipStatus,
         locked,
       ),
